3e5f601b23dcc095705298835b25d2c0b6be818a
Andrew Boyd <andrew@wearebraid.com>
2019-12-30 12:22:08 -0500

    update bootstrap script file to match docs

--- examples/pdfviewer.bootstrap.js
+++ examples/pdfviewer.bootstrap.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 
 (function () {
-  var mountTarget = document.getElementById('frb-pdf-viewer')
+  var mountTarget = document.getElementById('frb-pdf-viewer') // target your chosen mount point here
   var pdfViewerEl = document.createElement('div')
   pdfViewerEl.setAttribute('id', 'pdf-viewer')
   mountTarget.parentNode.insertBefore(pdfViewerEl, mountTarget)
 
-  // Mount the vue app when the window say's we're ready
+  // We're adding our render function to a global window object so that
+  // it can be called later when needed.
   window.frb.renderPDF = function () {
     var app = new Vue({
       el: '#pdf-viewer',
@@ -17,8 +18,12 @@
         return h('div', {}, [
           h(pdfviewer, {
             props: {
-              id: window.frb.docId,
-              solrResponse: window.frb.solrResponse
+              // pull our props from a global window object at the time
+              // that the function is called. This gives you time to prepare the data
+              // and then call window.frb.renderPDF() when everything is available.
+              documentPath: '/path/to/pdf/files', // this can be a local or remote URL
+              fileName: window.frb.docId,
+              solrResponse: window.frb.highlights
             }
           })
         ])
